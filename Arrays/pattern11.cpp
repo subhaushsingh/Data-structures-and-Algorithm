10afd809969b0ed93b3887df08bfe191cae6dd23
Subh <subhaushsingh@gmail.com>
2025-08-04 00:08:15 +0530

    square pattern problem

--- Arrays/pattern11.cpp
+++ Arrays/pattern11.cpp
@@ -34,9 +34,24 @@ void printPattern11(int n){
     }
 }
 
+
+void printPattern12(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==0||j==0||j==n-1||i==n-1){
+                cout<<"*";
+            }
+            else{
+                cout << " ";
+            }
+        }
+    }
+    cout << endl;
+}
+
 int main(){
     cout << "Enter the row:-";
     int row; cin >> row;
-    printPattern11(row);
+    printPattern12(row);
 
 }
