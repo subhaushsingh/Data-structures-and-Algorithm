7b9c0a191a9c06440ccf1f4cee595645ea504df4
Subh <subhaushsingh@gmail.com>
2025-08-26 23:32:01 +0530

    optimal approach for 2nd largest element

--- array_easy/secondLargestOptimal.cpp
+++ array_easy/secondLargestOptimal.cpp
@@ -3,19 +3,20 @@ using namespace std;
 
 
 int main(){
-    vector<int> arr={150,43,23,65,78};
+    vector<int> arr={150,43,223,65,8};
     int n=arr.size();
     int l=arr[0];
     int sl=INT_MIN;
     for(int i=1;i<n;i++){
         if(arr[i]>l){
+            sl=l;
             l=arr[i];
         }
-    }
-    for(int i=1;i<n;i++){
-        if(arr[i]>sl && arr[i]!=l){
+        else if(arr[i]<l && arr[i]>sl){
             sl=arr[i];
         }
     }
     cout << sl;
+
+
 }
