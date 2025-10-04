7bc5868b2083418824247aefca7a69f7f623b0bc
Subh Aush Singh <subhaushsingh@gmail.com>
2025-10-04 21:12:17 +0530

    using hash map for leetcode 169

--- array-medium/leetcode-169.cpp
+++ array-medium/leetcode-169.cpp
@@ -1,34 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-class Solution{
-    public:
-    int MajorityElement1(vector<int> nums,int target){
-        target=target/2;
-       int n=nums.size();
-       for(int i=0;i<n;i++){
-        int cnt=0;
-        for(int j=0;j<n;j++){
-            if(nums[j]==nums[i]){
-                cnt++;
+class Solution
+{
+public:
+    int MajorityElement1(vector<int> nums)
+    {
+        int target = nums.size() / 2;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            int cnt = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if (nums[j] == nums[i])
+                {
+                    cnt++;
+                }
             }
+            if (cnt > target)
+                return nums[i];
         }
-        if(cnt>target) return nums[i];
-
-       }
-       return -1; 
+        return -1;
     }
 
+    int MajorityElement2(vector<int> nums)
+    {
+        int n=nums.size();
 
+        map<int,int> mpp;
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
+        }
 
+        for (auto it : mpp) {
+        if (it.second > (n / 2)) {
+            return it.first;
+        }
+    }
+    return -1;
+    }
 };
 
-int main(){
-    vector<int> arr={2,2,1,1,1,2,2};
-    int n=arr.size();
+int main()
+{
+    vector<int> arr = {2, 2, 1, 1, 1, 2, 2};
+
     Solution obj;
-    int ans=obj.MajorityElement1(arr,n);
+    int ans = obj.MajorityElement2(arr);
     cout << ans;
-    
 }
