db35e79be9fb648aa91fb995072ca70ffec50f0c
Subh Aush Singh <subhaushsingh@gmail.com>
2025-10-04 22:42:37 +0530

    Moore's voting algorithm

--- array-medium/leetcode-169.cpp
+++ array-medium/leetcode-169.cpp
@@ -40,6 +40,33 @@ public:
     }
     return -1;
     }
+
+
+ int MajorityElement3(vector<int> nums){
+    int cnt=0;
+    int el;
+    
+    for(int i=0;i<nums.size();i++){
+        if(cnt==0){
+            cnt=1;
+            el=nums[i];
+        }
+        else if(nums[i]==el){
+            cnt++;
+        }
+        else{
+            cnt--;
+        }
+    }
+    int cnt1=0;
+    for(int i=0;i<nums.size();i++){
+        if(nums[i]==el) cnt1++;
+    }
+    if(cnt1>(nums.size()/2)){
+        return el;
+    }
+    return -1;
+ }
 };
 
 int main()
@@ -47,6 +74,6 @@ int main()
     vector<int> arr = {2, 2, 1, 1, 1, 2, 2};
 
     Solution obj;
-    int ans = obj.MajorityElement2(arr);
+    int ans = obj.MajorityElement3(arr);
     cout << ans;
 }
