046e7bb7b226d9d411ccb5316ac73f473b3e299a
Subh Aush Singh <subhaushsingh@gmail.com>
2025-10-06 18:38:06 +0530

    Updated kadane's algorithm to also give the subarray

--- array-medium/leetcode-53.cpp
+++ array-medium/leetcode-53.cpp
@@ -4,18 +4,30 @@ using namespace std;
 
 class Solution{
     public:
-    int MaxSubarray(vector<int> &nums){
+    vector<long long> MaxSubarray(vector<int> &nums){
         int n=nums.size();
         long long sum=0;
-        long long max_ele = INT_MIN;
+        long long max_ele = LLONG_MIN;
+
+        int start=0;
+        int low=-1,high=-1;
+
         for(int i=0;i<n;i++){
+
+            if(sum==0) start=i;
+
             sum+=nums[i];
-            max_ele=max(sum,max_ele);
+            if (sum > max_ele) {
+            max_ele = sum;
+
+            low = start;
+            high = i;
+        }
 
             if(sum<0) sum=0;
         }
 
-        return max_ele;
+        return {low,high,max_ele};
     }
 }; 
 
@@ -23,8 +35,14 @@ class Solution{
 int main(){
     vector<int> arr={-2,1,-3,4,-1,2,1,-5,4};
     Solution obj;
-    int ans=obj.MaxSubarray(arr);
-    cout << ans;
+    vector<long long> ans=obj.MaxSubarray(arr);
+    long long sum = ans[2];
+    cout << "SUM:" << sum<< endl;
+    for(int i=ans[0];i<=ans[1];i++){
+        cout << arr[i] << " ";
+    }
+
+    
 
 
 
