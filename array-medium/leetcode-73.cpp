fdd8b17817f640273b603d6b220ccfa26c67c811
Subh Aush Singh <subhaushsingh@gmail.com>
2025-10-09 17:51:23 +0530

    Optimal approach for setZero

--- array-medium/leetcode-73.cpp
+++ array-medium/leetcode-73.cpp
@@ -34,13 +34,60 @@ class Solution{
             }
         }
 
+        void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int colo = 1;
+
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    if(j!=0) matrix[0][j]=0;
+                    else colo=0;
+                }
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]!=0){
+                    if(matrix[0][j]==0||matrix[i][0]==0){
+                        matrix[i][j]=0;
+                    }
+                }
+            }
+        }
+
+        if(matrix[0][0]==0) {
+            for(int i=0;i<n;i++){
+                matrix[0][i]=0;
+            }
+        }
+
+        if(colo==0) {
+            for(int i=0;i<m;i++){
+                matrix[i][0]=0;
+            }
+        }
+
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                cout << matrix[i][j]<< " ";
+                }
+                cout << endl;
+            }
+        }
+
+    
     };
 
 
 int main(){
     vector<vector<int>> mat={{1,1,1},{1,0,1},{1,1,1}};
     Solution obj;
-    obj.setZeroes(mat,3,3);
+    obj.setZeroes(mat);
 
 
 
