d9e1bf8a8bc33c2544bc0fa8ab0f6d2b4ade503d
Subh Aush Singh <subhaushsingh@gmail.com>
2026-01-04 23:19:47 +0530

    leetcode 875 koko banana problem

---
+++ BinarySearch/leetcode-875.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1;
+        int high = *max_element(piles.begin(), piles.end());
+        int ans = high;
+
+         while (low <= high) {
+            int mid = low + (high - low) / 2;
+            long long hours = 0;
+
+            for (int b : piles) {
+                hours += (b + mid - 1) / mid; 
+            }
+
+            if (hours <= h) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
+
+
+int main(){
+
+    return 0;
+}
