5e24d5483df00cc07216336cbc0366c18802d141
Subh Aush Singh <subhaushsingh@gmail.com>
2026-01-06 23:41:56 +0530

    leetcode 1011

---
+++ BinarySearch/leetcode-1011.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    bool canShip(vector<int>& weights, int days, int cap) {
+        int currentLoad = 0;
+        int requiredDays = 1;
+
+        for (int w : weights) {
+            if (currentLoad + w > cap) {
+                requiredDays++;
+                currentLoad = w;
+            } else {
+                currentLoad += w;
+            }
+        }
+        return requiredDays <= days;
+    }
+
+    int shipWithinDays(vector<int>& weights, int days) {
+        int low = *max_element(weights.begin(), weights.end());
+        int high = accumulate(weights.begin(), weights.end(), 0);
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (canShip(weights, days, mid)) {
+                high = mid;    
+            } else {
+                low = mid + 1;   
+            }
+        }
+        return low;
+    }
+};
+
+
+int main(){
+
+
+
+
+};
