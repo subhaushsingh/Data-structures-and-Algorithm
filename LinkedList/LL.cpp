4d546d6a594d183eb7ef831ce9f17ab0417ae85f
Subh Aush Singh <subhaushsingh@gmail.com>
2026-02-01 20:16:40 +0530

    reversing linkedlist with 3 pointers

--- LinkedList/LL.cpp
+++ LinkedList/LL.cpp
@@ -2,32 +2,56 @@
 using namespace std;
 
 template <typename T>
-class Node{
-    public:
-        T data;
-        Node<T>* next;
-};
-
-template <typename T>
-class LL
-{
+class Node {
 public:
-    LL(T val);
-    void PrintLL(Node<T>* head);
-    ~LL();
+    T data;
+    Node<T>* next; //self referencing class
+
+    Node(T val) {
+        data = val;
+        next = nullptr;
+    }
 };
 
 template <typename T>
-LL<T>::LL(T val)
-{
-    data = val;
-    next = nullptr;
+class LL {
+private:
+    Node<T>* head;
+    void traverseRecursive(Node<T>* temp);
+    void traverseRecursiveReverse(Node<T>* temp);
+
+public:
+    LL();                 // constructor
+    void insert(T val);   // add node
+    void traverse();      // TRAVERSE
+    void traverseRecursive();
+    void traverseRecursiveReverse();
+    void reverseLL();
+    ~LL();                // destructor
+};
+
+template <typename T>
+LL<T>::LL() {
+    head = nullptr;
 }
 
 template <typename T>
-void LL<T>::PrintLL(Node<T>* head) {
+void LL<T>::insert(T val) {
+    Node<T>* newNode = new Node<T>(val);
+    if (!head) {
+        head = newNode;
+        return;
+    }
     Node<T>* temp = head;
-    while (temp) {
+    while (temp->next)
+        temp = temp->next;
+    temp->next = newNode;
+}
+
+template <typename T>
+void LL<T>::traverse() {
+    Node<T>* temp = head;
+    while (temp!=nullptr) {
         cout << temp->data << " ";
         temp = temp->next;
     }
@@ -35,9 +59,51 @@ void LL<T>::PrintLL(Node<T>* head) {
 }
 
 
+
+
 template <typename T>
-LL<T>::~LL()
-{
+void LL<T>::traverseRecursive(Node<T>* temp){
+    if(temp==nullptr) return;
+    cout << temp->data<<" ";
+    traverseRecursive(temp->next); 
+}
+template <typename T>
+void LL<T>::traverseRecursive(){
+    traverseRecursive(head);
+}
+
+
+template <typename T>
+void LL<T>::traverseRecursiveReverse(Node<T>* temp){
+    if(temp!=nullptr){ 
+    traverseRecursiveReverse(temp->next); 
+    cout << temp->data<<" ";}
+}
+template <typename T>
+void LL<T>::traverseRecursiveReverse(){
+    traverseRecursiveReverse(head);
+}
+
+template <typename T>
+void LL<T>::reverseLL(){
+    if (!head || !head->next) return;
+    Node<T>* s=head;
+    Node<T>* f = s->next;
+    Node<T>* h=f->next;
+    s->next = nullptr; 
+    while(f){
+        f->next=s;
+        s=f;
+        f=h;
+        if(h)
+            h=h->next;
+    }
+    head=s;
+}
+
+
+template <typename T>
+LL<T>::~LL() {
     Node<T>* curr = head;
     while (curr) {
         Node<T>* temp = curr;
@@ -48,4 +114,14 @@ LL<T>::~LL()
 
 
 
+int main() {
+    LL<int> list;
+    list.insert(10);
+    list.insert(20);
+    list.insert(30);
+    list.insert(40);
+    list.insert(50);
 
+    list.reverseLL();
+    list.traverse(); 
+}
