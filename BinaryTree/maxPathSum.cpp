2a40b27ffd05b1630ea13723d23ba0a683bf3c4e
Subh Aush Singh <subhaushsingh@gmail.com>
2026-04-19 23:59:04 +0530

    MaxPathSum

---
+++ BinaryTree/maxPathSum.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution{
+    private:
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x,TreeNode *left,TreeNode *right) : val(x), left(left),right(right) {}
+    };
+    public:
+    int maxPathSum(TreeNode* root) {
+        int maxi = INT_MIN;
+        maxPathDown(root,maxi);
+        return maxi;
+    }  
+    int maxPathDown(TreeNode* root,int &maxi){
+        if(root==nullptr) return 0;
+        int left = max(0,maxPathDown(root->left,maxi));
+        int right = max(0,maxPathDown(root->right,maxi));
+        maxi = max(maxi,left+right+root->val);
+        return max(left,right)+ root->val;
+    }
+};
+
+
+int main(){
+
+
+
+}
