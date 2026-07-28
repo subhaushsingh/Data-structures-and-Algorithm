6833f456a97ba3f89933df7ed9278703deca32c8
Subh Aush Singh <subhaushsingh@gmail.com>
2026-07-28 19:20:11 +0530

    Rotate String

---
+++ String/rotateString.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution{
+    public:
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length()) return false;
+        return (s + s).find(goal) != string::npos;
+    }
+    
+};
+
+
+int main(){
+    Solution sol;
+}
