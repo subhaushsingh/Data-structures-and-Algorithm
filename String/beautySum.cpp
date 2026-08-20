8eea100f498abcec3f70d18e11f759c6c3138e64
Subh Aush Singh <subhaushsingh@gmail.com>
2026-08-20 11:24:57 +0530

    power of 2

---
+++ String/beautySum.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        vector<string> words;
+        string temp = "";
+
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] != ' ') {
+                temp += s[i];
+            } else {
+                if (!temp.empty()) {
+                    words.push_back(temp);
+                    temp = "";
+                }
+            }
+        }
+
+
+         if (!temp.empty()) words.push_back(temp);
+
+        
+        reverse(words.begin(), words.end());
+
+        
+        string result = "";
+        for (int i = 0; i < words.size(); i++) {
+            result += words[i];
+            if (i != words.size() - 1) result += " ";
+        }
+
+        return result;
+    }
+};
